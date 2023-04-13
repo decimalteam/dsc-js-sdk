f8e93bdaa1db8c15b749a7bf4696596ec43fba8b
hakerkiler <vinatorul2@gmail.com>
2023-04-13 14:15:30 +0300

    1.1.17

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,8 @@ import Wallet from "./wallet";
 import Decimal from "./decimal";
 import TX_TYPE from "./txTypesNew";
 import { DecimalNetworks } from "./networks";
-import {
-  encodeAddresses,
-  encodeCosmosAccountAddress,
-  decodeCosmosAccountAddress,
-  verifyAddress,
-  createPublicKey,
-  txEncoderDecoder,
-  encodePubKey,
-} from "./utils/walletUtils";
 
 const Sdk = { Wallet, Decimal, TX_TYPE, DecimalNetworks };
 export default Sdk;
-export {
-  Wallet,
-  Decimal,
-  TX_TYPE,
-  DecimalNetworks,
-  encodeAddresses,
-  encodeCosmosAccountAddress,
-  decodeCosmosAccountAddress,
-  verifyAddress,
-  createPublicKey,
-  txEncoderDecoder,
-  encodePubKey,
-};
+export { Wallet, Decimal, TX_TYPE, DecimalNetworks };
+export * from "./utils/walletUtils";
