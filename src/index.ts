9a57f8f22b5af5a1874cbac7aa31ca92093438bd
hakerkiler <vinatorul2@gmail.com>
2023-04-24 13:46:17 +0300

    1.1.21 export pubkey

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import Wallet from "./wallet";
 import Decimal from "./decimal";
 import TX_TYPE from "./txTypesNew";
 import { DecimalNetworks } from "./networks";
+import { PubKey } from "./types/ethermint/crypto/v1/ethsecp256k1/keys";
 
-const Sdk = { Wallet, Decimal, TX_TYPE, DecimalNetworks };
+const Sdk = { Wallet, Decimal, TX_TYPE, DecimalNetworks, PubKey };
 export default Sdk;
 export { Wallet, Decimal, TX_TYPE, DecimalNetworks };
 export * from "./utils/walletUtils";
