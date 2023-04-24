9540985e200798e5026c4208848e8e230afd4347
hakerkiler <vinatorul2@gmail.com>
2023-04-24 14:47:33 +0300

    1.1.21

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,21 @@ import Decimal from "./decimal";
 import TX_TYPE from "./txTypesNew";
 import { DecimalNetworks } from "./networks";
 import { PubKey } from "./types/ethermint/crypto/v1/ethsecp256k1/keys";
+import {
+  makeAuthInfoBytes,
+  makeSignBytes,
+  makeSignDoc,
+} from "@cosmjs/proto-signing";
 
 const Sdk = { Wallet, Decimal, TX_TYPE, DecimalNetworks, PubKey };
 export default Sdk;
-export { Wallet, Decimal, TX_TYPE, DecimalNetworks };
+export {
+  Wallet,
+  Decimal,
+  TX_TYPE,
+  DecimalNetworks,
+  makeAuthInfoBytes,
+  makeSignBytes,
+  makeSignDoc,
+};
 export * from "./utils/walletUtils";
